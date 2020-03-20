b9acf47fdd20350d5bdc27b9d989fbf485d27eb7
Drew Patel <drew1patel@gmail.com>
2020-03-20 13:56:47 -0500

    created get and create task endpoints

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const functions = require("firebase-functions");
 const express = require("express"); // call express
 const cors = require("cors");
 const app = express(); // define our app using express
 const bodyParser = require("body-parser");
+const Onfleet = require("@onfleet/node-onfleet");
+const onfleet = new Onfleet(process.env.ONFLEET_KEY);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -11,10 +14,32 @@ app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 // ROUTES FOR OUR API
 // =============================================================================
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-app.get("/task", function(req, res) {
+app.get("/", function(req, res) {
   res.json({ message: "hooray! welcome to our api!" });
+});
+
+//this endpoint uses the shortID to identify query for packages
+//not the long id
+app.get("/task/:id", async function(req, res) {
+  const result = await onfleet.tasks.get(req.params.id, "shortId");
+  res.json(result);
+});
+
+app.post("/task", async function(req, res) {
+  const results = await onfleet.tasks.create({
+    destination: {
+      address: { unparsed: req.body.address }
+    },
+    recipients: [
+      {
+        name: req.body.name,
+        phone: req.body.phone
+      }
+    ],
+    notes: req.body.notes,
+    autoAssign: { mode: "distance" }
+  });
+  res.json(results);
 });
 
 // more routes for our API will happen here
