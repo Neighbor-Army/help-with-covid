5c768bac25e91833e0c46003bb802bf00edec35f
Drew Patel <drew1patel@gmail.com>
2020-03-20 13:20:20 -0500

    changed to express style endpoints for simplicity

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require("firebase-functions");
+const express = require("express"); // call express
+const cors = require("cors");
+const app = express(); // define our app using express
+const bodyParser = require("body-parser");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+// ROUTES FOR OUR API
+// =============================================================================
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+app.get("/task", function(req, res) {
+  res.json({ message: "hooray! welcome to our api!" });
 });
+
+// more routes for our API will happen here
+exports.widgets = functions.https.onRequest(app);
