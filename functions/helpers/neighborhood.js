7edb11d69040c751c340d814e8986265203e8a1a
Drew Patel <drew1patel@gmail.com>
2020-03-20 16:52:13 -0500

    moved functions to their own helper files and integrated neighborhood finding

---
+++ functions/helpers/neighborhood.js
@@ -0,0 +1,41 @@
+const axios = require("axios").default;
+getNeighborhood = async address => {
+  const randEmail =
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    "@" +
+    Date.now() +
+    ".com";
+  return axios
+    .post("https://nextdoor.com/ajax/account/validate/", {
+      address: {
+        street_address: address.streetAddress,
+        unit: address.unit,
+        city: address.city,
+        state: address.state,
+        zip_code: address.zipcode,
+        residence_id: null
+      },
+      email_address: randEmail
+    })
+    .catch(function(error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    })
+    .then(function(response) {
+      return response.data.context.neighborhood;
+    });
+};
+
+module.exports = {
+  getNeighborhood
+};
--- functions/index.js
+++ functions/index.js
@@ -4,9 +4,8 @@ const express = require("express"); // call express
 const cors = require("cors");
 const app = express(); // define our app using express
 const bodyParser = require("body-parser");
-const Onfleet = require("@onfleet/node-onfleet");
-const onfleet = new Onfleet(process.env.ONFLEET_KEY);
-
+const neighborhood = require("./helpers/neighborhood");
+const onfleet = require("./helpers/onfleet");
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,39 +13,58 @@ app.use(bodyParser.json());
 app.use(cors({ origin: true }));
 // ROUTES FOR OUR API
 // =============================================================================
-app.get("/", function(req, res) {
+
+/*
+address: {
+	unit: "apt 122",
+	number: "123",
+	street: "wentz st",
+	city: "Springfield",
+	state: "illinois",
+	postalCode: "65521"
+}
+
+person: {
+	name: "joe bill",
+	phone: "+14133333333"
+}
+
+*/
+
+app.get("/", async function(req, res) {
   res.json({ message: "hooray! welcome to our api!" });
 });
 
 app.get("/task/:id", async function(req, res) {
-  const result = await onfleet.tasks.get(req.params.id);
+  const result = await onfleet.getTask(req.param.id);
   res.json(result);
 });
 
 app.post("/task", async function(req, res) {
-  const results = await onfleet.tasks.create({
-    destination: {
-      address: { unparsed: req.body.address }
-    },
-    recipients: [
-      {
-        name: req.body.name,
-        phone: req.body.phone
-      }
-    ],
-    notes: req.body.notes,
-    autoAssign: { mode: "distance" }
+  const address = req.body.address;
+  const neighborhoodName = await neighborhood.getNeighborhood({
+    streetAddress: address.number + " " + address.street,
+    unit: address.apartment,
+    city: address.city,
+    state: address.state,
+    zipcode: address.postalCode
   });
+  console.log(neighborhoodName);
+  const results = await onfleet.createTask(
+    req.body.address,
+    req.body.person,
+    req.body.notes
+  );
   res.json(results);
 });
 
 app.patch("/task/:id", async function(req, res) {
-  const results = onfleet.tasks.update(req.params.id, req.body);
+  const results = await onfleet.updateTask(req.params.id, req.query.body);
   res.json(results);
 });
 
 app.delete("/task/:id", async function(req, res) {
-  const results = onfleet.tasks.deleteOne(req.params.id);
+  const results = onfleet.deleteTask(req.param.id);
   res.json(results);
 });
 
