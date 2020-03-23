0ce5daf6acedd9eea635a45a588d25edd71c968e
josemariodev <josemariodev@gmail.com>
2020-03-23 11:37:52 -0500

    Refactor payments using react-hooks-form

--- components/wizardForm/Payment.js
+++ components/wizardForm/Payment.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-const Payment = props => {
+const Payment = () => {
     const { register, handleSubmit, errors } = useForm();
     const onSubmit = data => console.log("data", data);
     console.log(errors);
@@ -11,31 +11,18 @@ const Payment = props => {
         <>
             <h3>Payment info</h3>
             <form onSubmit={handleSubmit(onSubmit)}>
+                <select name="payment type" ref={register({ required: true })}>
+                    <option value="venmo">venmo</option>
+                    <option value="paypal">paypal</option>
+                    <option value="cash">cash</option>
+                </select>
                 <input
                     type="text"
-                    placeholder="name"
-                    name="name"
-                    ref={register({ required: true, maxLength: 80 })}
-                />
-                <input
-                    type="tel"
-                    placeholder="phone"
-                    name="phone"
-                    ref={register({ required: true, maxLength: 12 })}
-                />
-                <input
-                    type="text"
-                    placeholder="address"
-                    name="address"
-                    ref={register({ required: true })}
-                />
-                <input
-                    type="text"
-                    placeholder="apartment"
-                    name="apartment"
+                    placeholder="notes"
+                    name="notes"
                     ref={register}
                 />
-                <button onClick={() => props.prevStep()}>previous</button>
+
                 <input type="submit" />
             </form>
         </>
