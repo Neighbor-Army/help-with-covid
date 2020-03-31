a9dac053e9ae75b046238a874afd4c09d0c9b5ac
Raz Luvaton <rluvaton@gmail.com>
2020-03-31 15:50:21 +0300

    docs(error-helper): add the default function in the jsdoc 📝

--- functions/utils/error-helper/index.js
+++ functions/utils/error-helper/index.js
@@ -4,7 +4,7 @@ const { generateUID } = require("../general");
  * Generate debugId to be able to search in the logs for all errors for the specific response
  * @param internalError The thrown error
  * @param newError The new error that about to throw
- * @param {function(): string} idGen ID Generator
+ * @param {function(): string} [idGen=generateUID] idGen ID Generator (The default is {@link generateUID})
  */
 const addDebugID = (internalError, newError, idGen = generateUID) => {
     internalError = internalError || {};
