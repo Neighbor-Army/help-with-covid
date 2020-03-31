fdc2fc15efef4abd59dd9ef46d9a580b1e397e40
Raz Luvaton <rluvaton@gmail.com>
2020-04-01 00:18:22 +0300

    feat(volunteer-success page): make the onfleet text a link ✨
    make the onfleet text a link depending on the platform browsing from

--- src/pages/volunteer-success.js
+++ src/pages/volunteer-success.js
@@ -1,11 +1,26 @@
 import React from "react";
+import ExternalLink from "../components/ExternalLink";
+import { isAndroid, isIOS } from "react-device-detect";
+
+const OnFleetLinks = {
+    // prettier-ignore
+    ANDROID: "https://play.google.com/store/apps/details?id=com.onfleet.driver.app",
+    IOS: "https://apps.apple.com/us/app/onfleet-driver/id1084013403",
+    DESKTOP: "https://onfleet.com/"
+};
 
 const VolunteerSuccessPage = () => {
+    // prettier-ignore
+    const onFleetLink = isIOS ? OnFleetLinks.IOS : isAndroid ? OnFleetLinks.ANDROID : OnFleetLinks.DESKTOP;
+
     return (
         <main>
             <h1>Thank you for volunteering!</h1>
             <p>You will receive a text message shortly.</p> <br />
-            <p>Please download OnFleet.</p>
+            <p>
+                Please download{" "}
+                <ExternalLink to={onFleetLink}>OnFleet</ExternalLink>.
+            </p>
             <style jsx>{`
                 main {
                     text-align: center;
